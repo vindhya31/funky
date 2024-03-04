0ee1b643a44b436a55c3b4780ad8b429be72f677
Vindhya Bhagavan <vindhya.bhagavan@gmail.com>
2024-03-04 15:31:03 +0100

    first commit

---
+++ src/Question1.java
@@ -0,0 +1,11 @@
+/*Days of the Week with switch case:
+        Write a program that allows the user to enter a number between 1 and 7 and
+        the program should print the corresponding day of the week using a switch case.
+        Bonus : Solve the same task using an array instead of a switch case.*/
+
+public class Question1 {
+    public static void main(String[] args) {
+        int a = 0;
+
+    }
+}
